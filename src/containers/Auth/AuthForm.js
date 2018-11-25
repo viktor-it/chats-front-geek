beec054906174f94ae509112a9cd3e390a23a7b7
Igor <superpike@mail.ru>
2018-11-25 15:40:30 +0500

    Собрал проект после перехода с начального репозитория и обновления с другими разработчиками

--- src/containers/Auth/AuthForm.js
+++ src/containers/Auth/AuthForm.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const authForm = () => {
     return (
         <div>
-            Форма авторизации
+            <h2>Форма авторизации</h2>
+            <Link to='/register'>Форма регистрации </Link>
         </div>
     );
 }
