5e6359850022fcafb50062f61dc61aa064f7d1de
grhgrmgrhrm <grhgrmgrhrm@gmail.com>
2018-11-30 00:32:58 +0300

    Добавлена ссылка на авторизацию

--- src/components/sidebar/Main.js
+++ src/components/sidebar/Main.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom'
 
 import Chats from '../../containers/sidebar/chats/Chats'
 import Contacts from '../../containers/sidebar/contacts/Contacts'
+import Form from '../authform/Form'
 
 
 const Main = () => (
@@ -10,6 +11,7 @@ const Main = () => (
 		<Switch>
 			<Route path='/chats' component={Chats}/>
 			<Route path='/contacts' component={Contacts}/>
+            <Route path='/form' component={Form}/>
 		</Switch>
 	</main>
 )
