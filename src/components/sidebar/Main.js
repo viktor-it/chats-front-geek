2a4c85a683037cbb266cc3374c69df2e0440a0f6
superpike <30590036+superpike@users.noreply.github.com>
2018-12-04 10:46:08 +0500

    Вернул как было

--- src/components/sidebar/Main.js
+++ src/components/sidebar/Main.js
@@ -1,14 +1,8 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
-<<<<<<< HEAD
-import Chats from '../../containers/sidebar/chats/Chats'
-import Contacts from '../../containers/sidebar/contacts/Contacts'
-import Form from '../authform/Form'
-=======
 import Chats from '../../containers/sidebar/Chats'
 import Contacts from '../../containers/sidebar/Contacts'
->>>>>>> develop-upstream
 
 
 const Main = () => (
