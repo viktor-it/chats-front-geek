5a5637f7b775fff6f571d7d84666aac48320535f
darkside1102 <34370456+darkside1102@users.noreply.github.com>
2018-12-04 22:56:53 +0400

    Изменил компонент профиля, разбив его на несколько компонентов

--- src/components/profiles/ProfileUser.js
+++ src/components/profiles/ProfileUser.js
@@ -1,43 +1,34 @@
 import React, { Component } from "react";
+import ProfileUserTop from "./ProfileUserTop";
+import Link from "react-router-dom/es/Link";
+import ProfileClose from "./ProfileClose";
 
 export default class ProfileUser extends Component {
-
     render() {
         let about = 'информация о себе';
         if (props.about) {
             about = props.about;
         }
         return(
-            <div className="profile">
-                <div className="profile__top">
-                    <img src="" alt="user_foto"/>
-                    <h4>Мой профиль</h4>
-                </div>
-                <div className="profile__info">
-                    <p>{props.about.email}</p>
-                    <h3>{props.about.name}</h3>
-                    <button>Кнопка редактирования информации</button>
-                    <span>Данная информация будет видна всем пользователям в Pocket Messenger</span>
-                </div>
+            <div>
+                <ProfileUserTop/>
                 <div className="profile__about">
                     <h5>О себе:</h5>
-                    <button>Кнопка редактирования информации</button>
                     <div className="profile__about_info">
                         <p>{props.about.info}</p>
                         <ul>
                             <li><a href="#">Написать сообщение</a></li>
-                            <li><a href="#">Личный кабинет</a></li>
                         </ul>
                         <input type="checkbox"/>
                         <ul>
-                            <li><a href="#">Выйти из профиля</a></li>
-                            <li><a href="#">Очистить переписку</a></li>
-                            <li><a href="#">Удалить профиль</a></li>
+                            <li><Link to="/">Выйти из профиля</Link></li>
+                            <li><Link to="/">Очистить переписку</Link></li>
+                            <li><Link to="/">Удалить профиль</Link></li>
                         </ul>
                     </div>
                 </div>
                 <div className="profile__btn-close">
-                    <button>Кнопка закрытия окна профиля</button>
+                   <ProfileClose/>
                 </div>
             </div>
         )
