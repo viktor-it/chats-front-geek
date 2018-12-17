2daca0fba913e48b6eae7fe91e6e3be1d4d1d9af
Kozlovtseva <alina.lm@mail.ru>
2018-12-17 17:22:31 +0300

    changed hover in Sidebar, too overcomplicated - fixed

--- src/layouts/Sidebar.js
+++ src/layouts/Sidebar.js
@@ -6,65 +6,38 @@ import Header from '../components/sidebar/Header';
 import Main from '../components/sidebar/Main';
 
 
-
 import styles from  './Sidebar.module.css';
 
 
-class Sidebar extends React.Component {
-    state = { 
-        chatsHovered: false,
-        contactsHovered: false,
-        ChatsCondition: false,
-        ContactsCondition: false
-    }
+const Sidebar = () => {
+    return(
+        <section className={styles.Sidebar}>
+            {/* бургер */}
+            <Menu />
 
-    ChatsHover = () => {
-        this.setState({
-            ChatsCondition: !this.state.ChatsCondition
-        });
-    }
-
-    ContactsHover = () => {
-        this.setState({
-            ContactsCondition: !this.state.ContactsCondition
-        });
-    }
-
-    render() {
-        return(
-            <section className={styles.Sidebar}>
-                {/* бургер */}
-                <Menu />
-
-                {/* чаты vs контакты */}
-                <div className={styles.Tabs}>
-                    <div
-                        onMouseEnter={this.ChatsHover}
-                        onMouseLeave={this.ChatsHover}
-                    >
-                        <Header href="/chats">
-                            <i className={this.state.ChatsCondition ? styles.IconHovered + ' fas fa-envelope' : styles.Icon + ' fas fa-envelope'} /> 
-                            <span className={this.state.ChatsCondition ? styles.TextHovered : styles.Text}>Чаты</span>
-                        </Header>
-                    </div>
-                    <div
-                        onMouseEnter={this.ContactsHover}
-                        onMouseLeave={this.ContactsHover}
-                    >
-                        <Header href="/contacts">
-                            <i className={this.state.ContactsCondition ? styles.IconHovered + ' fas fa-user-friends' : styles.Icon + ' fas fa-user-friends'} />
-                            <span className={this.state.ContactsCondition ? styles.TextHovered : styles.Text}>Контакты</span>
-                        </Header>
-                    </div>
-                {/*списки*/}
+            {/* чаты vs контакты */}
+            <div className={styles.Tabs}>
+                <div className={styles.Tab}>
+                    <Header href="/chats">
+                        <i className={styles.Icon + ' fas fa-envelope'} />
+                        <span className={styles.Text}>Чаты</span>
+                    </Header>
                 </div>
-                
-                <Main />
+                <div className={styles.Tab}>
+                    <Header href="/contacts">
+                        <i className={styles.Icon + ' fas fa-user-friends'} />
+                        <span className={styles.Text}>Контакты</span>
+                    </Header>
+                </div>                
+            </div>
 
-            </section>
-        )
-    }
+            {/*списки, профили*/}
+            <Main />
+
+        </section>
+    )
 }
 
 
 export default Sidebar;
+
