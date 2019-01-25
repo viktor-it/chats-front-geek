70dc330ecb45faf4ef368c162d6653c797c5d045
Igor <superpike@mail.ru>
2019-01-26 00:57:44 +0500

    change for local server

--- src/store/axios-docs.js
+++ src/store/axios-docs.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://pocketmsg.ru:8888',
+    baseURL: 'localhost:8888',
     headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
         'token': localStorage.getItem('token')
